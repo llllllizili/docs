54f630fe588eda84732faa507ef3c355ae1297ef
lizili <cn.zili.lee@outlook.com>
2026-03-15 15:08:14 +0800

    Deploy: 2026-03-15 15:08:13

---
+++ assets/clone-CgBkBLJW.js
@@ -0,0 +1,1 @@
+import{a as r}from"./graph-CfIiU75g.js";var a=4;function n(o){return r(o,a)}export{n as c};
---
+++ assets/index.html-Bmq0XvAL.js
@@ -0,0 +1,26 @@
+import{_ as r,c as p,b as e,a,h as o,e as l,w as i,g as s,o as m}from"./app-Bs-HU_T4.js";const d={};function u(g,t){const n=s("RouteLink");return m(),p("div",null,[t[7]||(t[7]=e("h1",{id:"",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#"},[e("span")])],-1)),e("p",null,[t[1]||(t[1]=o("详情可直接看",-1)),t[2]||(t[2]=e("a",{href:"https://github.com/llllllizili/gitops-demo",target:"_blank",rel:"noopener noreferrer"},"项目结构",-1)),t[3]||(t[3]=o("，配合",-1)),l(n,{to:"/ops/GitOps/%E8%90%BD%E5%9C%B0%E6%8C%87%E5%8D%97.html"},{default:i(()=>[...t[0]||(t[0]=[o("落地指南",-1)])]),_:1}),t[4]||(t[4]=o("食用，更佳",-1))]),t[8]||(t[8]=e("blockquote",null,[e("p",null,"引用")],-1)),t[9]||(t[9]=e("ul",null,[e("li",null,[e("p",null,[e("a",{href:"https://icloudnative.io/posts/what-is-gitops/",target:"_blank",rel:"noopener noreferrer"},"GitOps 介绍")])]),e("li",null,[e("p",null,[e("a",{href:"https://about.gitlab.com/topics/gitops/",target:"_blank",rel:"noopener noreferrer"},"What is GitOps?")])])],-1)),t[10]||(t[10]=e("blockquote",null,[e("p",null,"本站")],-1)),e("ul",null,[e("li",null,[e("p",null,[l(n,{to:"/ops/GitOps/%E8%90%BD%E5%9C%B0%E6%8C%87%E5%8D%97.html"},{default:i(()=>[...t[5]||(t[5]=[o("落地指南",-1)])]),_:1})])]),e("li",null,[e("p",null,[l(n,{to:"/ops/GitOps/%E8%BF%90%E7%BB%B4%E5%B9%B3%E5%8F%B0%E5%BB%BA%E8%AE%BE%E6%B5%81%E7%A8%8B%E5%92%8C%E8%A7%84%E8%8C%83%E5%A4%A7%E7%BA%B2.html"},{default:i(()=>[...t[6]||(t[6]=[o("运维平台建设流程和规范大纲",-1)])]),_:1})])])]),a(` 
+> 实操
+
+- 基础设施
+    - [Metallb](./metallb.md)
+    - [Ningx-ingress](./ingress.md)
+    - [Longhorn](./longhorn.md)
+- 基础工具安装
+    - [minio](../../cloud-native/middleware/minio.md)
+    - [gitlab](./gitlab.md)
+    - [gitlab-runner](./gitlab-runner.md)
+    - [harbor](./harbor.md)
+    - [argocd](./argocd.md)
+    - [grafana](./grafana.md)
+    - [loki](./loki.md)
+    - 监控- 待定
+- [项目编排](https://github.com/llllllizili/gitops-demo)
+- 程序编写
+    - [hello world（go）](https://github.com/llllllizili/gitops-demo/tree/main/product/go-demo)
+- 研发相关
+    - 代码提交
+    - 配置管理
+    - 需求联动
+- 编排编写
+    - 编排设计
+    - 编排使用 `)])}const h=r(d,[["render",u],["__file","index.html.vue"]]),f=JSON.parse(`{"path":"/ops/GitOps/","title":"GitOps","lang":"zh-CN","frontmatter":{"article":false,"title":"GitOps","icon":null,"order":11,"index":false,"description":"详情可直接看项目结构，配合食用，更佳 引用 GitOps 介绍 What is GitOps? 本站","head":[["meta",{"property":"og:url","content":"https://docs.lizili.online/ops/GitOps/"}],["meta",{"property":"og:site_name","content":"zll's Blog"}],["meta",{"property":"og:title","content":"GitOps"}],["meta",{"property":"og:description","content":"详情可直接看项目结构，配合食用，更佳 引用 GitOps 介绍 What is GitOps? 本站"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-03-12T03:42:17.000Z"}],["meta",{"property":"article:author","content":"zll"}],["meta",{"property":"article:modified_time","content":"2024-03-12T03:42:17.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"GitOps\\",\\"description\\":\\"详情可直接看项目结构，配合食用，更佳 引用 GitOps 介绍 What is GitOps? 本站\\"}"]]},"headers":[],"git":{"createdTime":1680403823000,"updatedTime":1710214937000,"contributors":[{"name":"lizili","email":"cn.zili.lee@outlook.com","commits":9}]},"readingTime":{"minutes":0.59,"words":178},"filePathRelative":"ops/GitOps/README.md","localizedDate":"2023年4月2日","excerpt":"\\n<p>详情可直接看<a href=\\"https://github.com/llllllizili/gitops-demo\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">项目结构</a>，配合<a href=\\"/ops/GitOps/%E8%90%BD%E5%9C%B0%E6%8C%87%E5%8D%97.html\\" target=\\"_blank\\">落地指南</a>食用，更佳</p>\\n<blockquote>\\n<p>引用</p>\\n</blockquote>\\n<ul>\\n<li>\\n<p><a href=\\"https://icloudnative.io/posts/what-is-gitops/\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">GitOps 介绍</a></p>\\n</li>\\n<li>\\n<p><a href=\\"https://about.gitlab.com/topics/gitops/\\" target=\\"_blank\\" rel=\\"noopener noreferrer\\">What is GitOps?</a></p>\\n</li>\\n</ul>","autoDesc":true}`);export{h as comp,f as data};
